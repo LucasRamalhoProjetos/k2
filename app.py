98dc76549d8b0689c6505c0afd2c8f4b1b7f7f33
Lucas Ramalho <lucashateen@gmail.com>
2025-03-04 14:03:12 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+# Título da página
+st.title("Meu Modo de Pensar para uma IA")
+
+# Texto principal
+st.write("""
+Neste espaço, abordarei os conceitos que estou aprendendo e compartilharei meu ponto de vista sobre como entender a mente humana para formular uma inteligência artificial. O objetivo é explorar não apenas a criação de uma IA, mas também a compreensão das lacunas e complexidades que envolvem o pensamento humano.
+
+Antes de qualquer coisa existir, ela não existe – seja uma ideia, um conhecimento, um objeto ou até mesmo o próprio universo. Mas então surge a dúvida, o questionamento, a inquietação.
+O não existir, a dúvida e o existir não são apenas etapas isoladas – eles compõem o próprio ciclo que rege tudo ao nosso redor. Da menor descoberta pessoal ao surgimento de civilizações, tudo segue esse fluxo contínuo.
+
+Foi justamente porque um dia o universo não existia que ele pôde vir a existir. É só porque não sabemos algo, que podemos aprender. É só porque duvidamos que sejamos capazes de criar, evoluir e transformar. A dúvida não é um obstáculo, mas o motor que nos impulsiona.
+
+Se aplico essa lógica à minha visão de Deus, então Deus é esse tripé essencial: o que existe, o que não existe e a própria dúvida sobre qual dos dois será. Isso faz sentido porque tudo no universo pode ser reduzido a essas três possibilidades fundamentais. Para mim, Deus é o princípio binário que estrutura a realidade.
+
+Não podemos limitar Deus a uma imagem. Ele é o todo. Podemos não ser iguais no olhar e no reconhecer, mas na redução atômica, tudo e todos somos parecidos. Somos irmãos, feitos à Sua imagem e semelhança. Do pó viemos e ao pó retornaremos. Para alguns, essa perspectiva pode não fazer sentido, mas, para mim, faz todo o sentido.
+
+Admiro o fato que, mesmo sendo ateu, você reconhece a importância da religião e da crença. Gostaria, um dia, de ter a oportunidade de conversar com o senhor.
+""")
